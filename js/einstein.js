3f160edaf78810966cf4efd2ef4e1d7cc8c4704a
Anna O <usune@mail.ru>
2022-06-02 01:05:35 +0300

    my energy

---
+++ js/einstein.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const myWeight = 54;
+const speedLight = 3e8;
+
+const myEnergy = myWeight*(speedLight**2);
+
+const myEnergyTwo = myEnergy.toExponential(3);
+
+console.log(`E = ${myEnergy} Дж`);
+console.log(`E = ${myEnergyTwo} Дж`);
+
+console.log(`E = ${typeof myEnergy} Дж`);
--- js/script.js
+++ js/script.js
@@ -3,11 +3,6 @@
 const checkbox = document.querySelector(".add-product__checkbox");
 const checkInput = document.querySelectorAll(".add-product__input")[3];
 
-const productName = ['Хлеб', 'Майонез', 'Порошок стиральный', 'Пиво', 'Водка'];
-const productCount = [2, 1, 1, 3, 1];
-const productCategory = ['Продукты питания', 'Бытовая химия', 'Алкоголь'];
-const productPrice = [76, 100, 360, 85, 200];
-
 checkbox.addEventListener("change", () => {
   if (checkbox.checked) {
     checkInput.disabled = false;
@@ -16,6 +11,12 @@ checkbox.addEventListener("change", () => {
   }
 });
 
+
+
+/*const productName = ['Хлеб', 'Майонез', 'Порошок стиральный', 'Пиво', 'Водка'];
+const productCount = [2, 1, 1, 3, 1];
+const productCategory = ['Продукты питания', 'Бытовая химия', 'Алкоголь'];
+const productPrice = [76, 100, 360, 85, 200];
 let allSum = 0;
 
 const productSum = productCount.forEach((e, i) => {
@@ -24,5 +25,22 @@ const productSum = productCount.forEach((e, i) => {
 });
 
 console.log("Товары:", productName);
-console.log("Общая сумма:", allSum);
+console.log("Общая сумма:", allSum);*/
 
+
+const productName = prompt("Наименование товара");
+const productCount = +prompt("Количество товара");
+const productCategory = prompt("Категория товара");
+const productPrice = +prompt("Цена товара");
+
+console.log(`Наименование товара: ${productName}`);
+console.log(`Количество товара: ${productCount}`);
+console.log(`Категория товара: ${productCategory}`);
+console.log(`Цена товара: ${productPrice}`);
+
+console.log(`Наименование товара: ${typeof productName}`);
+console.log(`Количество товара: ${typeof productCount}`);
+console.log(`Категория товара: ${typeof productCategory}`);
+console.log(`Цена товара: ${typeof productPrice}`);
+
+console.log(`На складе ${productCount} единицы товара "${productName}" на сумму ${productPrice*productCount} деревянных`);
